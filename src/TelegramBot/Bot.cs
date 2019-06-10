55de7f74997014a27198d3de4ad85595093b03ee
Captious99 <48892069+Captious99@users.noreply.github.com>
2019-06-10 18:24:50 +0300

    Create TelegramBot

---
+++ src/TelegramBot/Bot.cs
@@ -0,0 +1,24 @@
+﻿using Telegram.Bot;
+using Newtonsoft.Json;
+
+namespace TelegramBot
+{
+    public static class Bot
+    {
+        private static ITelegramBotClient botClient;
+
+        public static void InitilizeBot()
+        {
+            string token = ReadToken();
+            botClient = new TelegramBotClient(token);
+        }
+
+        private static string ReadToken()
+        {
+            string jsonText = System.IO.File.ReadAllText("bot_data.json");
+            dynamic botData = JsonConvert.DeserializeObject<dynamic>(jsonText);
+            string token = botData.Token;
+            return token;
+        }
+    }
+}
