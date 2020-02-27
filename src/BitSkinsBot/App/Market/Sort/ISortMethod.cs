dc427348447d776ea129ae6892273c2a3caf17cc
dmitrydnl <48892069+Captious99@users.noreply.github.com>
2020-02-27 18:54:45 +0300

    Create ISortMethod.cs

---
+++ src/BitSkinsBot/App/Market/Sort/ISortMethod.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace BitSkinsBot.FastMarketAnalize
+{
+    internal interface ISortMethod
+    {
+        void Sort(List<BitSkinsApi.Market.MarketItem> marketItems, SortFilter searchFilter);
+    }
+}
